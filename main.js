fa95df19a176c86a3a9b741a480c9edc107f0f35
Murad <muradrustamov@gmail.com>
2019-05-27 10:30:39 +0400

    Opening specific tab via link / Also checking if random string after # in link

--- main.js
+++ main.js
@@ -22,7 +22,12 @@ function selectNewTab(classes) {
 
 function showActiveTab(classes, tab) {
   var active, current;
-  if (tab !== null && tab !== undefined) {
+  if (getTabId()) {
+    active = $(".tab-content__" + getTabId()).length
+      ? getTabId()
+      : getFirstTab(classes.tab);
+    $(classes.tab + "[data-tab=" + active + "]").addClass("active");
+  } else if (tab !== null && tab !== undefined) {
     current = tab;
     active = current.data("tab");
   } else {
@@ -34,9 +39,26 @@ function showActiveTab(classes, tab) {
     current.each(function() {
       if ($(this).hasClass("active")) {
         active = $(this).data("tab");
+      } else {
+        active = getFirstTab(classes.tab);
+        $("[data-tab=" + active + "]").addClass("active");
       }
     });
   }
-  console.log(active);
+
   $(".tab-content__" + active).show();
+}
+
+// Get TabID from Url
+function getTabId() {
+  var url = window.location.href;
+
+  var strStart = url.indexOf("#") !== -1 ? url.indexOf("#") + 1 : false;
+
+  return strStart ? url.slice(strStart, url.length) : false;
+}
+
+// Get First Tab Name
+function getFirstTab(selector) {
+  return $(selector + ":nth-child(1)").data("tab");
 }
